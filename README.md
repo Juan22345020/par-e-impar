e3b984f21acd75b4b5a669ecfe5cff92107bb3b2
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-03-06 16:37:00 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # par-e-impar
+
+# Analisis
+
+# Diseño
+
+## Diagrama de flujo
+
+![Diagrama de flujo](Diagrama.png "Diagrama de flujo")
---
+++ par-e-impar.py
@@ -0,0 +1,21 @@
+
+print("------------------------------------")
+print("---------PAR O IMPAR----------------")
+print("------------------------------------")
+
+# input
+
+
+a = int(input("Digite el numero :"))
+
+a = a % 2
+
+# procesing
+
+if (a==0) :
+    print("el numero es par")
+else :
+    print("el numero es impar")
+    
+
+
